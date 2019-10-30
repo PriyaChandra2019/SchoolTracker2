8180fc2af12ebcb4c897e1ad95dbc93de017f76f
Priya Chandra <ChandruPriya@gmail.com>
2019-10-30 16:05:40 -0700

    worked on errrors & debugging

--- SchoolTracker2/Program.cs
+++ SchoolTracker2/Program.cs
@@ -6,45 +6,73 @@ using System.Threading.Tasks;
 
 namespace SchoolTracker1
 {
+    enum school
+    {
+        Hogwarts,
+        Harvard,
+        MIT 
+    }
+
     class Program
     {
+        static List<student> students = new List<student>();
+
         static void Main(string[] args)
         {
-            var students = new List<student>();
 
             var adding = true;
 
             while (adding)
             {
-                var newStudent = new student();
+                try
+                {
 
-                newStudent.Name = Util.Console.Ask("Student Name: ");
-                                                
-                newStudent.Grade = int.Parse(Util.Console.Ask("Student Grade: "));
-                                
-                newStudent.Birthday = Util.Console.Ask("Student Birthday: ");
+                    var newStudent = new student();
 
-                newStudent.Address = Util.Console.Ask("Student Address: ");
+                    newStudent.Name = Util.Console.Ask("Student Name: ");
 
-                newStudent.Phone = int.Parse(Util.Console.Ask("Student phone Number: "));
+                    newStudent.Grade = Util.Console.AskInt("Student Grade: ");
 
-                students.Add(newStudent);
-                student.Count++;
-                Console.WriteLine("Student Count: {0}", student.Count);
+                    newStudent.School = (school) Util.Console.AskInt("School Name (type the corresponding number):\n 0: Hogwarts High \n 1: Harvard \n 2: MIT \n)");
+
+                    newStudent.Birthday = Util.Console.Ask("Student Birthday: ");
+
+                    newStudent.Address = Util.Console.Ask("Student Address: ");
+
+                    newStudent.Phone = Util.Console.AskInt("Student phone Number: ");
+
+                    students.Add(newStudent);
+                    student.Count++;
+                    Console.WriteLine("Student Count: {0}", student.Count);
 
 
-                Console.WriteLine("Add another? y/n");
+                    Console.WriteLine("Add another? y/n");
 
-                if (Console.ReadLine() != "y")
-                    adding = false;
+                    if (Console.ReadLine() != "y")
+                        adding = false;
+
+                }
+
+
+
+                catch (FormatException msg)
+                {
+                    Console.WriteLine(msg.Message);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Error adding student, Please try again");
+                }
+
+
             }
-
 
             foreach (var student in students)
 
             {
                 Console.WriteLine("Name: {0}, Grade: {1} ", student.Name, student.Grade);
             }
+            Exports();
             Console.ReadKey();
         }
 
@@ -53,61 +81,75 @@ namespace SchoolTracker1
             var importedStudent = new student("Tim", 98, "birthday", "address", 4664654);
             Console.WriteLine(importedStudent.Name);
         }
-    }
 
-    class Member
-    {
-        public string Name;
-        public string Address;
-        protected int phone;
-    }
-
-
-    class student : Member
-    {
-        static public int Count = 0;
-                
-        public int Grade;
-        public string Birthday;
-        
-       
-
-        public student()
+        static void Exports()
         {
+            foreach (var student in students)
+            {
+                switch (student.School)
+                {
+                    case school.Hogwarts:
+                        Console.WriteLine("Exporting to Hogwarts");
+                        break;
+                    case school.Harvard:
+                        Console.WriteLine("Exporting to Harvard");
+                        break;
+                    case school.MIT:
+                        Console.WriteLine("Exporting to MIT");
+                        break;
+
+                }
+            }
+        }
+
+        class Member
+        {
+            public string Name;
+            public string Address;
+            protected int phone;
+
+            public int Phone
+            {
+                set { phone = value; }
+            }
 
         }
 
-        public student(string name, int grade, string birthday, string address, int phone)
-        {
-            Name = name;
-            Grade = grade;
-            Birthday = birthday;
-            Address = address;
-            Phone = phone;
 
-        
+        class student : Member
+        {
+            static public int Count = 0;
+
+            public int Grade;
+            public string Birthday;
+            public school School;
+
+
+
+            public student()
+            {
+
+            }
+
+            public student(string name, int grade, string birthday, string address, int phone)
+            {
+                Name = name;
+                Grade = grade;
+                Birthday = birthday;
+                Address = address;
+                Phone = phone;
+
+
+            }
+
+
         }
 
-        public int Phone
+        class Teacher : Member
         {
-            set { phone = value; }
+
+            public string Subject;
         }
-
-        public void SetPhone(int number)
-        {
-            phone = number;
-        }
-
-
-
     }
-
-    class Teacher
-    {
-        public string Name;
-        public string Address;
-        public int phone;
-        public string Subject;
-    }
-
 }
+
